e2b2ff2ad15eb167a7cbf61e086b66574852a138
xyz <290082124@qq.com>
2019-01-24 13:53:50 +0800

    change something

--- gateway/src/main/kotlin/com/xyz/gateway/GatewayWebApplication.kt
+++ gateway/src/main/kotlin/com/xyz/gateway/GatewayWebApplication.kt
@@ -3,9 +3,11 @@ package com.xyz.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.stereotype.Controller
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Controller
 class GatewayWebApplication
 
 fun main(args: Array<String>) {
